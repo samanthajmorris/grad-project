9c5316c15b56078a5410527c82a19b0552d16eb5
samanthajmorris <39094057+samanthajmorris@users.noreply.github.com>
2020-03-03 11:46:12 -0400

    Add files via upload
    Update file outputs

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import docker
 import multiprocessing
 
 #TODO: Change the domain variable to the domain you would like to scan
-domain = "scanme.nmap.org"
+#domain = "scanme.nmap.org"
+domain = "www.acadiau.ca"
 #TODO: Change the home_dir variable to the path to the folder that you'd like the output files written to. Note: End with a forward slash
 home_dir = '/Users/samanthamorris/beachcomber-output/'
 
@@ -17,8 +18,8 @@ def nikto_function():
 #Runs SSLYZE in a container and outputs a text file to the home directory containing the SSLYZE report.
 def sslyze_function():
     client = docker.from_env()
-    result = client.containers.run("nablac0d3/sslyze", "--regular " + domain, remove=True, tty=True)
-    f = open(home_dir + domain + '_sslyze_report.txt', 'wb')
+    result = client.containers.run("nablac0d3/sslyze", "--json_out=- --regular " + domain, remove=True, tty=True)
+    f = open(home_dir + domain + '_sslyze_report.json', 'wb')
     f.write(result)
     print("SSLYZE Analysis Completed.")
 
@@ -33,12 +34,12 @@ def nmap_function():
 #Runs Nmap in a container and outputs a text file to the home directory containing the Nmap report.
 def nmap_function2():
     client = docker.from_env()
-    container = client.containers.run("instrumentisto/nmap", " -A " + domain + "-oX /home/nmap.xml", detach=True, tty=True)
-    f = open(home_dir + domain + '_nmap_report.tar', 'wb')
+    container = client.containers.run("instrumentisto/nmap", " -A " + domain + " -oX /home/nmap.xml", detach=True, tty=True)
+    f = open(home_dir + domain + '_nmap_report.xml', 'wb')
     flag = 0
     while(flag == 0):
         if(container.status == 'exited'):
-            bits, stat = container.get_archive('./')
+            bits, stat = container.get_archive('/home/nmap.xml')
             flag = 1
         else:
             container.reload()
@@ -50,7 +51,7 @@ def nmap_function2():
 def pshtt_function():
     client = docker.from_env()
     container = client.containers.run("18fgsa/domain-scan", domain + " --scan=pshtt", detach=True, tty=True)
-    f = open(home_dir + domain + '_pshtt_report.tar', 'wb')
+    f = open(home_dir + domain + '_pshtt_report.csv', 'wb')
     flag = 0
     while(flag == 0):
         if(container.status == 'exited'):
@@ -67,18 +68,18 @@ if __name__ == "__main__":
     #Warning: Nikto takes ~10 minutes to run
     #p1 = multiprocessing.Process(target=nikto_function)
 
-    p2 = multiprocessing.Process(target=sslyze_function)
-    p3 = multiprocessing.Process(target=nmap_function2)
+    #p2 = multiprocessing.Process(target=sslyze_function)
+    #p3 = multiprocessing.Process(target=nmap_function2)
     p4 = multiprocessing.Process(target=pshtt_function)
 
     #p1.start()
-    p2.start()
-    p3.start()
+    #p2.start()
+    #p3.start()
     p4.start()
 
     #p1.join()
-    p2.join()
-    p3.join()
+    #p2.join()
+    #p3.join()
     p4.join()
     print("Finished Analysis.")
 
